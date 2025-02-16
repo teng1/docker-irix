e4ac2dc0ec253213271ee4d2331f536245e5f729
tom <tom@tom-ThinkPad-X1-Carbon-2nd.broadband>
2025-02-16 09:29:04 +0000

    xinetd rsh tested

--- src/debian/docker-entrypoint.sh
+++ src/debian/docker-entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 echo 1 > /proc/sys/net/ipv4/ip_no_pmtu_disc
 echo "2048 32767" > /proc/sys/net/ipv4/ip_local_port_range
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
 
 /usr/bin/supervisord -c /etc/supervisord.conf
 
